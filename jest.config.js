657047edb0ac39984a5eccc5111b253c92245620
Vova <bordiian.v@gmail.com>
2023-03-27 10:12:26 +0300

    prepared for starting to learn about test

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/**/*.test.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.test.ts'],
   verbose: true,
   forceExit: true,
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true,
+  // clearMocks: true
 };
