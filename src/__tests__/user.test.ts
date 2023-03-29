e2ef9f2e60c0d4a0ac2e1f5d646cef5c02f01842
Vova <bordiian.v@gmail.com>
2023-03-29 12:54:19 +0300

    creating plan and skeleton for start user testing

---
+++ src/__tests__/user.test.ts
@@ -0,0 +1,29 @@
+describe('user', () => {
+  // user registration
+
+  describe('user registration', () => {
+    describe('given the username and password are valid', () => {
+      it('shouls retuen the user payload', () => {
+
+      })
+    })
+
+    describe('given the passwords do not match', () => {
+      it('should return a 400', () => {
+
+      })
+    })
+
+    describe('given the user service throws', () => {
+      it('should return 409 error', () => {
+
+      })
+    })
+  })
+    // the username password get validation
+    // verify that the passwords must match
+    // verify that the handler handles any errors
+
+  // creating a user session
+    // a user can log in with a valid email and password
+})
